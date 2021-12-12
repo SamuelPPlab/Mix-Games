58c99250dd81a2b83557b0b95409a9b37a30918a
SamuelPPlab <samuelpp.s@hotmail.com>
2021-12-11 23:31:41 -0300

    Componente GameRegisterPage criado.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import Login from './pages/Login';
+import GameRegisterPage from './pages/GameRegisterPage';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/register-game" element={<GameRegisterPage />} />
     </Routes>
   );
 };
---
+++ src/pages/GameRegisterPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";´
+
+const GameRegisterPage = () => {
+  return(
+    <div>
+      
+    </div>
+  );
+};
+
+export default GameRegisterPage;
