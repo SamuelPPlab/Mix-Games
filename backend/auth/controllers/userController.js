9b05551be12cc69db570481fde2f2c8c63f0f667
SamuelPPlab <samuelpp.s@hotmail.com>
2021-12-15 19:41:32 -0300

    Camada de controlers feita

---
+++ backend/auth/controllers/userController.js
@@ -0,0 +1,50 @@
+const { Router } = require('express');
+const { createUser, findUserByEmail } = require('../models/userModel');
+const { fieldFinder, isEmailRegistered, emailValidator } = require('../services/validators');
+const { BAD_REQUEST, CONFLICT, CREATED, SUCCESS } = require('../services/httpStatuses');
+const { invalidEntries, emailAlreadyExists, userNotRegistered, wrongPassword } = require('../services/messages');
+const tokenCreator = require('../auth/tokenCreator');
+
+const UserController = new Router();
+
+UserController.post('/create', async (req, res) => {
+  const { email, password, name } = req.body;
+  const requiredFields = ['name', 'password', 'email'];
+  const doRequiredFieldsExist = fieldFinder(req.body, requiredFields);
+  const isEmailValid = emailValidator(email);
+
+  if (!doRequiredFieldsExist || !isEmailValid) {
+    return res.status(BAD_REQUEST).json(invalidEntries);
+  }
+
+  const emailExists = await isEmailRegistered(email);
+
+  if (emailExists) {
+    return res.status(CONFLICT).json(emailAlreadyExists);
+  }
+
+  const { insertedId } = await createUser(name, email, password);
+  const userContainer = { user: { _id: insertedId, name, email, password } };
+
+  return res.status(CREATED).json(userContainer);
+});
+
+UserController.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  const userRegistered = await findUserByEmail(email);
+  
+  if (!userRegistered) {
+    return res.status(BAD_REQUEST).json(userNotRegistered);
+  }
+
+  if (userRegistered.password !== password) {
+    return res.status(BAD_REQUEST).json(wrongPassword);
+  }
+
+  const { _id: id } = userRegistered;
+
+  const token = tokenCreator({ id, email, password });
+  return res.status(SUCCESS).json({ token });
+});
+
+module.exports = UserController;
